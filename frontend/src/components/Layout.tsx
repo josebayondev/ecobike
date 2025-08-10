5cacd1224a047e62c147610567da68869d853f45
Jose Bayon <josebayondev@gmail.com>
2025-08-10 11:28:40 +0200

    Agregando opacidad a las img en el Layout para mejorar la visibilidad

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,12 +4,16 @@ import rightBike from "../assets/img/rightBike.png";
 import logo from "../assets/img/logo.png";
 import fondo from "../assets/img/fondo.jpg";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-6 relative">
       <h1 className="text-5xl font-bold text-center mb-6 text-gray-800">
@@ -34,13 +38,17 @@ export default function Layout({ children }: LayoutProps) {
       <img
         src={leftBike}
         alt="Bicicleta izquierda"
-        className="absolute left-26 top-1/2 transform -translate-y-1/2 w-100 h-auto opacity-90 z-5"
+        className={`absolute left-26 top-1/2 transform -translate-y-1/2 w-100 h-auto z-5 ${
+          isHomePage ? 'opacity-60' : 'opacity-40'
+        }`}
       />
       {/* Imagen derecha */}
       <img
         src={rightBike}
         alt="Bicicleta derecha"
-        className="absolute right-10 top-1/2 transform -translate-y-1/2 w-125 h-auto opacity-90 z-5"
+        className={`absolute right-10 top-1/2 transform -translate-y-1/2 w-125 h-auto z-5 ${
+          isHomePage ? 'opacity-80' : 'opacity-40'
+        }`}
       />
 
       {/* Contenido central */}
